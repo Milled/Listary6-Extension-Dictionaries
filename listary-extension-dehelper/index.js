11927193e13d894f552a290465fc551e0b19f74f
Jaho Zheng <Milled@users.noreply.github.com>
2020-12-22 21:13:36 +0100

    Add files via upload

---
+++ listary-extension-dehelper/index.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+
+async function search(query) {
+    const response = await axios({
+        url: 'https://www.godic.net/dicts/prefix/'+query,
+    });
+    const json = response.data;
+    // console.log(json);
+    if (json) {
+        return json.map(each=>{
+            return {title:
+                each.value + ' ' + each.label
+                }
+            }
+            );
+    }
+    else {
+        return [{
+            title: "urls"
+        }];
+    }
+}
+
+module.exports = {
+    search: search
+};
